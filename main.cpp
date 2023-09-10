743cfe187d23407ee2286efa3c44772e77673075
DerDieDenDas <126321270+DerDieDenDas@users.noreply.github.com>
2023-09-10 16:12:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,78 @@
-#include "mylib.h" // Įterpiamas vartotojo apibrėžtas "mylib.h" antraštės failas.
-#include "file_functions.h" // Įterpiamas kitas vartotojo apibrėžtas "file_functions.h" antraštės failas.
+#include "mylib.h"          // Įtraukiama savo sukurtų bibliotekų byla
+#include "file_functions.h" // Įtraukiama failų valdymo funkcijų byla
 
-int paz_skaicius; // Globalus kintamasis skirtas pažymių skaičiui.
+int paz_skaicius;
 
-int main()
-{
-    // Kintamieji, skirti vartotojo pasirinkimams fiksuoti. Pradinė reikšmė yra -1.
-    int uzkl_1 = -1, uzkl_2 = -1, uzkl_3 = -1, uzkl_4 = -1, uzkl_5 = -1, uzkl_6 = -1;
-    Vector<Studentas> grupe; // Kintamasis grupės studentų saugojimui.
-    Studentas temp; // Laikinas "Studentas" objektas.
-    double visa_trukme = 0; // Kintamasis tam tikrų operacijų trukmės saugojimui.
+int main() {
+    int uzkl_1, uzkl_2, uzkl_3, uzkl_4, uzkl_5, uzkl_6; // Deklaruojami kintamieji programos veikimo kontrolės reikmėms
+    Vector<Studentas> grupe; // Sukuriamas vektorius, skirtas laikyti Studentas tipo objektus
+    Studentas temp; // Sukuriamas laikinas Studentas tipo objektas
+    double visa_trukme = 0; // Inicializuojamas kintamasis, skirtas matuoti visos programos veikimo trukmę
 
-    // Vartotojo pasirinkimai
-    cout << "Ar norite: ..."; // Klausiama vartotojo apie duomenų įvedimo būdą.
-    int_input_check(uzkl_1,(Vector<int>){1, 2, 3}); // Tikrinamas įvedimas. Užtikrinama, kad pasirinkimas yra tinkamas.
-    // ... Panašūs vartotojo pasirinkimai ir jų patikra.
+    // Meniu pasirinkimams
+    cout << "Ar norite: \n 1. Įvesti vardus ir pažymius (įveskite '1')\n"
+            " 2. Generuoti vardus ir pažymius atsitiktinai (įveskite '2')\n"
+            " 3. Skaityti vardus ir pažymius iš failo (įveskite '3')\n";
+    int_input_check(uzkl_1, {1, 2, 3}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
 
-    switch(uzkl_1) { // Pasirenkama pagal pirmąjį pasirinkimą.
+    cout << "\nAr norite:\n 1. Skaičiuoti vidurkį (įveskite '1')\n"
+            " 2. Skaičiuoti medianą (įveskite '2')\n"
+            " 3. Skaičiuoti medianą ir vidurkį (įveskite '3')\n"
+            " 4. Spausdinti pažymius (įveskite '4')\n";
+    int_input_check(uzkl_2, {1, 2, 3, 4}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+
+    if (uzkl_1 == 2) {
+        // Jei generuojami pažymiai, klausti, ar juos išvesti į ekraną arba į failą
+        cout << "\nAr norite: \n 1. Išvesti pažymius į ekraną (įveskite '1')\n"
+                " 2. Išvesti rezultatus į failą (įveskite '2')\n";
+        int_input_check(uzkl_3, {1, 2}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+    }
+
+    if (uzkl_1 == 3 || uzkl_3 == 2) {
+        // Jei skaitomi arba generuojami studentai, klausti, ar juos skirstyti į protingus ir tingius
+        cout << "\nAr norite skirstyti studentus į mokslinčius ir tinginius? Įveskite '1', jei taip, '0', jei ne.\n";
+        int_input_check(uzkl_5, {1, 0}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+    }
+
+    if (uzkl_5 == 1) {
+        // Jei skirstomi studentai, klausti, kokią strategiją naudoti
+        cout << "\nĮveskite '1', jei norite naudoti 1 strategiją (du nauji konteineriai), arba '2', jei norite naudoti 2 strategiją (vienas naujas konteineris).\n";
+        int_input_check(uzkl_6, {1, 2}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+    }
+
+    switch (uzkl_1) {
         case 1:
-            stud_ivest(grupe,uzkl_2); // Jei pasirinkimas yra 1, rankiniu būdu įvedami studentų duomenys.
+            stud_ivest(grupe, uzkl_2); // Įvedimas: vardai ir pažymiai
             break;
         case 3:
-            visa_trukme += failo_nuskaitymas(grupe,uzkl_2); // Jei pasirinkimas yra 3, studentų duomenys yra nuskaitomi iš failo.
+            visa_trukme += failo_nuskaitymas(grupe, uzkl_2); // Skaitomi vardai ir pažymiai iš failo
             break;
         default:
-            visa_trukme += stud_gen(grupe,temp,uzkl_2); // Numatytoji reikšmė (tikriausiai 2): generuojami studentų duomenys.
+            visa_trukme += stud_gen(grupe, temp, uzkl_2); // Generuojami studentai
             break;
     }
 
-    // Priklausomai nuo ankstesnių pasirinkimų, duomenys arba rodomi ekrane, arba saugomi faile.
+    if (uzkl_1 != 3 && uzkl_3 != 2) {
+        // Jei rezultatai nėra rašomi į failą, juos išvesti į ekraną
+        isvedimas_i_ekrana(grupe, uzkl_4, uzkl_2, uzkl_1); // Rezultatų išvedimas į ekraną
+    } else {
+        if (uzkl_5 == 0) {
+            // Jei studentai neskirstomi, rezultatus rašyti į failą
+            Timer print; // Laiko matavimo pradžia
+            if (uzkl_2 == 4) {
+                spausd_i_faila(grupe, uzkl_1, uzkl_2, "studentai" + to_string(grupe.size()) + ".txt"); // Spausdinimas į failą
+            } else {
+                spausd_i_faila(grupe, uzkl_1, uzkl_2, "output.txt"); // Spausdinimas į failą
+            }
+            cout << "Studentų duomenų spausdinimas į failą užtruko: " << print.elapsed() << "s\n"; // Laiko matavimo pabaiga
+            visa_trukme += print.elapsed(); // Atnaujinama bendra veikimo trukmė
+        } else {
+            // Studentai skirstomi pagal pasirinktą strategiją
+            skirstymas(uzkl_6, uzkl_2, uzkl_1, grupe, visa_trukme); // Studentų skirstymas
+        }
+        cout << "Visas programos darbas truko: " << visa_trukme << "s\n"; // Visos programos veikimo trukmė
+    }
 
-    cout << "Visas programos darbas truko: "<< visa_trukme << "s\n"; // Rodomas bendras programos vykdymo laikas.
-
-    system("pause"); // Programa sustabdoma kol vartotojas nespaus klavišo.
+    system("pause"); // Laukimas, kol naudotojas spaus paspaus ENTER
+    // return 0; // Programos pabaiga, grąžinamas nulinis kodas
 }
-  // return 0; // Programos pabaiga, grąžinamas nulinis kodas
-// }
