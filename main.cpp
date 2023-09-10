0cea8415ce3fe6e28f0f4ee43f5f684fb000af3a
DerDieDenDas <126321270+DerDieDenDas@users.noreply.github.com>
2023-09-10 18:56:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,78 +1,83 @@
-#include "mylib.h"          // Įtraukiama savo sukurtų bibliotekų byla
-#include "file_functions.h" // Įtraukiama failų valdymo funkcijų byla
+#include "mylib.h"                       // Įtraukiamos bibliotekos, kurios yra reikalingos kode.
+#include "file_functions.h"
 
-int paz_skaicius;
+const std::string STUDENTS_FILE_PREFIX = "studentai";  // Konstantos su studentų failo prefiksu.
+const std::string OUTPUT_FILE = "output.txt";          // Konstantos su išvesties failo pavadinimu.
 
-int main() {
-    int uzkl_1, uzkl_2, uzkl_3, uzkl_4, uzkl_5, uzkl_6; // Deklaruojami kintamieji programos veikimo kontrolės reikmėms
-    Vector<Studentas> grupe; // Sukuriamas vektorius, skirtas laikyti Studentas tipo objektus
-    Studentas temp; // Sukuriamas laikinas Studentas tipo objektas
-    double visa_trukme = 0; // Inicializuojamas kintamasis, skirtas matuoti visos programos veikimo trukmę
+// Funkcija skirta užklausoms gauti iš vartotojo.
+void selectInput(Vector<Studentas>& grupe, int& uzkl_1, int& uzkl_2, int& uzkl_3, int& uzkl_5, int& uzkl_6) {
+    // Pirmoji užklausa: kaip gauti duomenis.
+    cout << "Ar norite: \n 1. Ivesti vardus ir pazymius (iveskite '1')\n 2. Generuoti vardus ir pazymius atsitiktinai (iveskite '2')\n 3. Skaityti vardus ir pazymius is failo (iveskite '3')\n";
+    int_input_check(uzkl_1, {1, 2, 3});  // Tikrina ar vartotojo įvestis yra viena iš galimų reikšmių.
 
-    // Meniu pasirinkimams
-    cout << "Ar norite: \n 1. Įvesti vardus ir pažymius (įveskite '1')\n"
-            " 2. Generuoti vardus ir pažymius atsitiktinai (įveskite '2')\n"
-            " 3. Skaityti vardus ir pažymius iš failo (įveskite '3')\n";
-    int_input_check(uzkl_1, {1, 2, 3}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+    // Antroji užklausa: kokį veiksmą atlikti su gautais duomenimis.
+    cout << "\nAr norite:\n 1. Skaiciuoti vidurki (iveskite '1')\n 2. Skaiciuoti mediana (iveskite '2')\n 3. Skaiciuoti mediana ir vidurki (iveskite '3')\n 4. Spausdinti pazymius (iveskite '4')\n";
+    int_input_check(uzkl_2, {1, 2, 3, 4});
 
-    cout << "\nAr norite:\n 1. Skaičiuoti vidurkį (įveskite '1')\n"
-            " 2. Skaičiuoti medianą (įveskite '2')\n"
-            " 3. Skaičiuoti medianą ir vidurkį (įveskite '3')\n"
-            " 4. Spausdinti pažymius (įveskite '4')\n";
-    int_input_check(uzkl_2, {1, 2, 3, 4}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
-
+    // Trečioji užklausa: kur rodyti rezultatus, jei buvo pasirinkta generuoti duomenis.
     if (uzkl_1 == 2) {
-        // Jei generuojami pažymiai, klausti, ar juos išvesti į ekraną arba į failą
-        cout << "\nAr norite: \n 1. Išvesti pažymius į ekraną (įveskite '1')\n"
-                " 2. Išvesti rezultatus į failą (įveskite '2')\n";
-        int_input_check(uzkl_3, {1, 2}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+        cout << "\nAr norite: \n 1. Isvesti pazymius i ekrana (iveskite '1')\n 2. Isvesti rezultata i faila (iveskite '2')\n";
+        int_input_check(uzkl_3, {1, 2});
     }
 
+    // Ketvirtoji užklausa: ar norima skirstyti studentus pagal jų pasiekimus.
     if (uzkl_1 == 3 || uzkl_3 == 2) {
-        // Jei skaitomi arba generuojami studentai, klausti, ar juos skirstyti į protingus ir tingius
-        cout << "\nAr norite skirstyti studentus į mokslinčius ir tinginius? Įveskite '1', jei taip, '0', jei ne.\n";
-        int_input_check(uzkl_5, {1, 0}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+        cout << "\nAr norite skirstyti studentus i mokslincius ir tinginius? Iveskite '1', jei taip, '0', jei ne.\n";
+        int_input_check(uzkl_5, {1, 0});
     }
 
+    // Penktoji užklausa: kurią strategiją naudoti studentų skirstymui.
     if (uzkl_5 == 1) {
-        // Jei skirstomi studentai, klausti, kokią strategiją naudoti
-        cout << "\nĮveskite '1', jei norite naudoti 1 strategiją (du nauji konteineriai), arba '2', jei norite naudoti 2 strategiją (vienas naujas konteineris).\n";
-        int_input_check(uzkl_6, {1, 2}); // Nuskaitomas ir patikrinamas naudotojo pasirinkimas
+        cout << "\nIveskite '1', jei norite naudoti 1 strategija (du nauji konteineriai) arba '2', jei norite naudoti 2 strategija (vienas naujas konteineris).\n";
+        int_input_check(uzkl_6, {1, 2});
     }
+}
 
+// Pagrindinė programa.
+int main() {
+    Vector<Studentas> grupe;  // Vektorius, kuriame saugomi studentai.
+    Studentas temp;           // Laikinas studentas naudojamas generavimui.
+    double visa_trukme = 0;  // Kintamasis saugantis programos vykdymo trukmę.
+
+    // Užklausų kintamieji.
+    std::array<int, 6> uzklusos;
+    uzklusos.fill(-1);  // Pradinis užklausų kintamųjų užpildymas -1 reikšme.
+    int& uzkl_1 = uzklusos[0];
+    int& uzkl_2 = uzklusos[1];
+    int& uzkl_3 = uzklusos[2];
+    int& uzkl_4 = uzklusos[3];
+    int& uzkl_5 = uzklusos[4];
+    int& uzkl_6 = uzklusos[5];
+
+    selectInput(grupe, uzkl_1, uzkl_2, uzkl_3, uzkl_5, uzkl_6);  // Užklausų funkcijos iškvietimas.
+
+    // Pagal pirmosios užklausos rezultatą nustatoma, kaip gauti duomenis.
     switch (uzkl_1) {
         case 1:
-            stud_ivest(grupe, uzkl_2); // Įvedimas: vardai ir pažymiai
+            stud_ivest(grupe, uzkl_2);  // Duomenų įvedimas ranka.
             break;
         case 3:
-            visa_trukme += failo_nuskaitymas(grupe, uzkl_2); // Skaitomi vardai ir pažymiai iš failo
+            visa_trukme += failo_nuskaitymas(grupe, uzkl_2);  // Duomenų skaitymas iš failo.
             break;
         default:
-            visa_trukme += stud_gen(grupe, temp, uzkl_2); // Generuojami studentai
+            visa_trukme += stud_gen(grupe, temp, uzkl_2);  // Duomenų generavimas.
             break;
     }
 
-    if (uzkl_1 != 3 && uzkl_3 != 2) {
-        // Jei rezultatai nėra rašomi į failą, juos išvesti į ekraną
-        isvedimas_i_ekrana(grupe, uzkl_4, uzkl_2, uzkl_1); // Rezultatų išvedimas į ekraną
-    } else {
+    // Rezultatų išvedimas.
+    if (uzkl_1 != 3 && uzkl_3 != 2)  // Jei rezultatai turėtų būti rodomi ekrane.
+        isvedimas_i_ekrana(grupe, uzkl_4, uzkl_2, uzkl_1);
+    else {  // Jei rezultatai turėtų būti išsaugoti faile.
         if (uzkl_5 == 0) {
-            // Jei studentai neskirstomi, rezultatus rašyti į failą
-            Timer print; // Laiko matavimo pradžia
-            if (uzkl_2 == 4) {
-                spausd_i_faila(grupe, uzkl_1, uzkl_2, "studentai" + to_string(grupe.size()) + ".txt"); // Spausdinimas į failą
-            } else {
-                spausd_i_faila(grupe, uzkl_1, uzkl_2, "output.txt"); // Spausdinimas į failą
-            }
-            cout << "Studentų duomenų spausdinimas į failą užtruko: " << print.elapsed() << "s\n"; // Laiko matavimo pabaiga
-            visa_trukme += print.elapsed(); // Atnaujinama bendra veikimo trukmė
+            Timer print;  // Laikmatis, matuojantis išvedimo į failą trukmę.
+            spausd_i_faila(grupe, uzkl_1, uzkl_2, (uzkl_2 == 4) ? STUDENTS_FILE_PREFIX + std::to_string(grupe.size()) + ".txt" : OUTPUT_FILE);  // Išvedimas į failą.
+            cout << "Studentu duomenu spausdinimas i faila uztruko: " << print.elapsed() << "s\n";
+            visa_trukme += print.elapsed();  // Pridedama išvedimo trukmė prie bendros programos trukmės.
         } else {
-            // Studentai skirstomi pagal pasirinktą strategiją
-            skirstymas(uzkl_6, uzkl_2, uzkl_1, grupe, visa_trukme); // Studentų skirstymas
+            skirstymas(uzkl_6, uzkl_2, uzkl_1, grupe, visa_trukme);  // Studentų skirstymo funkcija.
         }
-        cout << "Visas programos darbas truko: " << visa_trukme << "s\n"; // Visos programos veikimo trukmė
+        cout << "Visas programos darbas truko: " << visa_trukme << "s\n";  // Bendros programos trukmės išvedimas.
     }
 
-    system("pause"); // Laukimas, kol naudotojas spaus paspaus ENTER
-    // return 0; // Programos pabaiga, grąžinamas nulinis kodas
+    system("pause");  // Laikina programa, kol vartotojas nuspaudžia klavišą.
 }
